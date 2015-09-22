34557532df9ffcbe79c10ef9df74ae9f3bf34fa8
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2015-09-22 20:40:50 +0200

    Members section (empty)
    Implemented as a single page app

--- server.js
+++ server.js
@@ -3,5 +3,16 @@ var express = require('express');
 var app = express();
 //Por defecto, redirigimos las peticiones al archivo con el mismo nombre de la carpeta static_content
 app.use(express.static(__dirname + '/static_content'));
+//Es posible solicitar los datos de los usuarios en formato JSON
+var fs = require("fs");
+app.get('/API/users', function(req, res, next) {
+	var file = fs.readFileSync("data/users.json" , "utf8");
+    var users= JSON.parse(file);
+	for(var i=0;i<users.length;i++){
+		users[i].id=i;
+	}
+    res.json(users);
+});
+
 //Iniciamos el servidor en el puerto que provee Azure, u 8080 si estamos probando en local
 app.listen(process.env.PORT || 8080);
